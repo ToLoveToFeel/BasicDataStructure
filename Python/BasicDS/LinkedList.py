b0c3aaa1812f33ffed3f760e01dfa89f0feabc3a
i-love-linux <1137247975@qq.com>
2020-02-13 20:51:21 +0800

    LinkedList 全局变量改为私有变量

--- Python/BasicDS/LinkedList.py
+++ Python/BasicDS/LinkedList.py
@@ -15,23 +15,23 @@ class LinkedList:
     查找操作：get(index):O(n)    contains(e):O(n)
     """
     def __init__(self):
-        self._dummyHead = self.__Node()
-        self._size = 0
+        self.__dummyHead = self.__Node()
+        self.__size = 0
 
     # 获取链表中的元素的个数
     def getSize(self):
-        return self._size
+        return self.__size
 
     # 判断链表是否为空
     def isEmpty(self):
-        return self._size == 0
+        return self.__size == 0
 
     # 在链表的index(0-based)位置添加新的元素e
     # 在链表中不是一个常用的操作，练习用
     def add(self, index, e):
-        assert not (index < 0 or index > self._size), "Add failed. Illegal index."
+        assert not (index < 0 or index > self.__size), "Add failed. Illegal index."
 
-        prev = self._dummyHead
+        prev = self.__dummyHead
         for i in range(index):
             prev = prev.next
 
@@ -39,7 +39,7 @@ class LinkedList:
         # node.next = prev.next()
         # prev.next = node
         prev.next = self.__Node(e, prev.next)
-        self._size += 1
+        self.__size += 1
 
     # 在链表头添加元素
     def addFirst(self, e):
@@ -47,14 +47,14 @@ class LinkedList:
 
     # 在链表的末尾添加新的元素e
     def addLast(self, e):
-        self.add(self._size, e)
+        self.add(self.__size, e)
 
     # 获得链表的第index(0-based)个位置的元素
     # 在链表中不是一个常用的操作，练习用
     def get(self, index):
-        assert not (index < 0 or index >= self._size), "Add failed. Illegal index."
+        assert not (index < 0 or index >= self.__size), "Add failed. Illegal index."
 
-        cur = self._dummyHead.next
+        cur = self.__dummyHead.next
         for i in range(index):
             cur = cur.next
         return cur.e
@@ -65,20 +65,20 @@ class LinkedList:
 
     # 获取链表的最后一个元素
     def getLast(self):
-        return self.get(self._size - 1)
+        return self.get(self.__size - 1)
 
     # 修改链表的第ndex(0-based)个位置的元素为e
     # 在链表中不是一个常用的操作，练习用
     def set(self, index, e):
-        assert not (index < 0 or index >= self._size), "Add failed. Illegal index."
-        cur = self._dummyHead.next
+        assert not (index < 0 or index >= self.__size), "Add failed. Illegal index."
+        cur = self.__dummyHead.next
         for i in range(index):
             cur = cur.next
         cur.e = e
 
     # 在链表中查找是否有元素e
     def contains(self, e):
-        cur = self._dummyHead
+        cur = self.__dummyHead
         while cur != None:
             if cur.e == e:
                 return True
@@ -88,15 +88,15 @@ class LinkedList:
     # 从链表删除index(0-based)位置的元素，返回删除的元素
     # 在链表中不是一个常用的操作，练习用
     def remove(self, index):
-        assert not (index < 0 or index >= self._size), "Add failed. Illegal index."
-        prev = self._dummyHead
+        assert not (index < 0 or index >= self.__size), "Add failed. Illegal index."
+        prev = self.__dummyHead
         for i in range(index):
             prev = prev.next
 
         retNode = prev.next
         prev.next = retNode.next
         retNode.next = None
-        self._size -= 1
+        self.__size -= 1
 
         retValue = retNode.e
         del(retNode)
@@ -108,11 +108,11 @@ class LinkedList:
 
     # 从链表中删除最后一个元素，返回删除的元素
     def removeLast(self):
-        return self.remove(self._size - 1)
+        return self.remove(self.__size - 1)
 
     # 从链表中删除元素e
     def removeElement(self, e):
-        prev = self._dummyHead
+        prev = self.__dummyHead
         while prev.next != None:
             if prev.next.e == e:
                 break
@@ -122,14 +122,14 @@ class LinkedList:
             delNode = prev.next
             prev.next = delNode.next
             del delNode
-            self._size -= 1
+            self.__size -= 1
 
     def __str__(self):
         return self.getString()
 
     def getString(self):
         res = ""
-        cur = self._dummyHead.next
+        cur = self.__dummyHead.next
         while cur != None:
             res = res + str(cur.e) + "->"
             cur = cur.next
