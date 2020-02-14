b828b39f8a35942d0e008bbe1a919b6bcb760253
i-love-linux <1137247975@qq.com>
2020-02-14 12:21:39 +0800

    添加基于堆的优先队列

---
+++ Python/BasicDS/PriorityQueue.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+from BasicDS.MaxHeap import MaxHeap
+from BasicDS.MinHeap import MinHeap
+
+
+class PriorityQueue:
+
+    def __init__(self, capacity=10, arr=[], isMaxHeap=True):
+        if isMaxHeap:
+            self.__data = MaxHeap(capacity=capacity, arr=arr)
+        else:
+            self.__data = MinHeap(capacity=capacity, arr=arr)
+
+    # 获取队列大小
+    def getSize(self):
+        return self.__data.size()
+
+    # 判断队列是否为空
+    def isEmpty(self):
+        return self.__data.isEmpty()
+
+    # 入队
+    def enqueue(self, e):
+        self.__data.add(e)
+
+    # 元素出队，同时返回队首元素
+    def dequeue(self):
+        return self.__data.extract()
+
+    # 查看队首元素，不出队
+    def getFront(self):
+        return self.__data.find()
+
